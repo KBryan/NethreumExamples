96933e74778005d0781651a9388dc6cba97f516c
kwame.eth <kwame@nebulachain.io>
2024-08-05 08:27:56 -0400

    Wallets.cs
    An example of creating an in-game wallet in Nethereum C#

---
+++ Wallets.cs
@@ -0,0 +1,35 @@
+using System;
+using Nethereum.Hex.HexConvertors.Extensions;
+using Nethereum.Signer;
+using Nethereum.HdWallet;
+using NBitcoin;
+
+namespace WalletGenerator
+{
+    class Wallets
+    {
+        static void Main(string[] args)
+        {
+            // Generate a random private key
+            var ecKey = EthECKey.GenerateKey();
+            var privateKey = ecKey.GetPrivateKeyAsBytes().ToHex();
+            Console.WriteLine($"Private Key: {privateKey}");
+
+            // Generate mnemonic from entropy
+            var entropy = ecKey.GetPrivateKeyAsBytes();
+            var mnemonic = new Mnemonic(Wordlist.English, entropy);
+            Console.WriteLine($"Mnemonic: {mnemonic}");
+
+            // Derive private key from mnemonic
+            var wallet = new Wallet(mnemonic.ToString(), null);
+            var account = wallet.GetAccount(0);
+            var derivedPrivateKey = account.PrivateKey;
+            Console.WriteLine($"Derived Private Key: {derivedPrivateKey}");
+
+            // Generate the Ethereum Address
+            var address = ecKey.GetPublicAddress();
+            Console.WriteLine($"Ethereum Address: {address}");
+
+        }
+    }
+}
