efdc3d8fe57294ffc02ab4802f46066b1c2a377f
kwame.eth <kwame@nebulachain.io>
2024-08-05 09:01:01 -0400

    Update Wallets.cs

--- Wallets.cs
+++ Wallets.cs
@@ -1,4 +1,3 @@
-using System;
 using Nethereum.Hex.HexConvertors.Extensions;
 using Nethereum.Signer;
 using Nethereum.HdWallet;
@@ -10,26 +9,32 @@ namespace WalletGenerator
     {
         static void Main(string[] args)
         {
-            // Generate a random private key
-            var ecKey = EthECKey.GenerateKey();
-            var privateKey = ecKey.GetPrivateKeyAsBytes().ToHex();
-            Console.WriteLine($"Private Key: {privateKey}");
+            try
+            {
+                // Generate a random private key
+                var ecKey = EthECKey.GenerateKey();
+                var privateKey = ecKey.GetPrivateKeyAsBytes().ToHex();
+                Console.WriteLine($"Private Key: {privateKey}");
 
-            // Generate mnemonic from entropy
-            var entropy = ecKey.GetPrivateKeyAsBytes();
-            var mnemonic = new Mnemonic(Wordlist.English, entropy);
-            Console.WriteLine($"Mnemonic: {mnemonic}");
+                // Generate mnemonic from entropy
+                var entropy = ecKey.GetPrivateKeyAsBytes();
+                var mnemonic = new Mnemonic(Wordlist.English, entropy);
+                Console.WriteLine($"Mnemonic: {mnemonic}");
 
-            // Derive private key from mnemonic
-            var wallet = new Wallet(mnemonic.ToString(), null);
-            var account = wallet.GetAccount(0);
-            var derivedPrivateKey = account.PrivateKey;
-            Console.WriteLine($"Derived Private Key: {derivedPrivateKey}");
+                // Derive private key from mnemonic
+                var wallet = new Wallet(mnemonic.ToString(), null);
+                var account = wallet.GetAccount(0);
+                var derivedPrivateKey = account.PrivateKey;
+                Console.WriteLine($"Derived Private Key: {derivedPrivateKey}");
 
-            // Generate the Ethereum Address
-            var address = ecKey.GetPublicAddress();
-            Console.WriteLine($"Ethereum Address: {address}");
-
+                // Generate the Ethereum Address
+                var address = ecKey.GetPublicAddress();
+                Console.WriteLine($"Ethereum Address: {address}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
         }
     }
 }
